a625e2833c7223bf29f2cb17ea3dcafbc8a7e5be
ostkaka0 <john.ostkaka@gmail.com>
2014-02-03 12:52:30 +0100

    ...

--- 3d voxel game/StateTest.cpp
+++ 3d voxel game/StateTest.cpp
@@ -2,15 +2,22 @@
 
 #include "Game.h"
 #include "IRenderer.h"
+#include "VoxelMatrix.h"
 
 void StateTest::Load(Game *game, EventHandler *eventHandler)
 {
+	voxels = new VoxelMatrix(16, 16, 16);
 
+	voxels->setVoxel(3, 3, 3, reinterpret_cast<IVoxel*>(1));
+	voxels->setVoxel(4, 3, 3, reinterpret_cast<IVoxel*>(1));
+	voxels->setVoxel(5, 3, 3, reinterpret_cast<IVoxel*>(1));
+	voxels->setVoxel(5, 4, 3, reinterpret_cast<IVoxel*>(1));
+	voxels->setVoxel(3, 3, 5, reinterpret_cast<IVoxel*>(1));
 }
 
 void StateTest::Draw(Game *game, IRenderer *renderer)
 {
-
+	//renderer->RenderMatrix(voxels);
 }
 
 void StateTest::Update(Game *game)
--- 3d voxel game/StateTest.h
+++ 3d voxel game/StateTest.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "State.h"
 
+class VoxelMatrix;
+
 class StateTest : public State
 {
+	VoxelMatrix *voxels;
 public:
 	virtual void Load(Game *game, EventHandler *eventHandler);
 	virtual void Draw(Game *game, IRenderer *renderer);
